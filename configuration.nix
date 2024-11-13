a6c63b0afb624aaa9f1349f2771258453eedf64e
makeitworkAndris <andrisbarath5@gmail.com>
2024-11-13 13:31:00 +0100

    +genymotion -anbox

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,6 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_xanmod;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -101,7 +100,7 @@
       strawberry-qt6
       filezilla
       baobab
-      anbox
+      genymotion
     ];
   };
   nixpkgs.config.permittedInsecurePackages = [
